2b24bc3026d4678639279f5484ac92d2901b6ba0
slbaber <slbaber2@gmail.com>
2020-06-03 17:19:51 -0400

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+
+          let newSatellite: Satellite;
+ 
+          let fetchedSatellites = data.satellites;
+          for (let sat of fetchedSatellites) {
+            newSatellite = new Satellite(sat.name, sat.type, sat.operational, sat.orbitType, sat.launchDate)
+            this.sourceList.push(newSatellite);
+          }
+ 
+       }.bind(this));
+    }.bind(this));
+ 
+  }
+ 
 }
